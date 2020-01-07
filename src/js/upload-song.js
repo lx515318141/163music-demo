56cb4eadd034c9618aefbdfc81efe1387e4843e8
lixiang <515318141@qq.com>
2020-01-08 00:33:33 +0800

    丰富管理员页面

---
+++ src/js/upload-song.js
@@ -0,0 +1,18 @@
+{
+    let view = {
+        el: '.uploadArea',
+        template: ``,
+        render(data){
+            $(this.el).html(this.template)
+        }
+    }
+    let model = {}
+    let controller{
+        init(view, model){
+            this.view = view
+            this.model = model
+            this.view.render(this.model.data)
+        }
+    }
+    controller.init(view, model)
+}
