ecea5910b034e4438a3f6a3364451199b565d245
lixiang <515318141@qq.com>
2020-01-15 00:25:55 +0800

    首页HTML及css初步完成

---
+++ src/js/index/tabs.js
@@ -0,0 +1,26 @@
+{
+    let view = {
+        el: '#tabs',
+        init(){
+            this.$el = $(this.el)
+        },
+        
+    }
+    let model = {}
+    let controller = {
+        init(view, model){
+            this.view = view
+            this.view.init()
+            this.model = model
+            this.bindEvents()
+
+        },
+        bindEvents(){
+            this.view.$el.on('click', '.tabs-nav > li', (e)=>{
+                let $li = $(e.currentTarget)
+                $li.addClass('active').siblings().removeClass('active')
+            })
+        }
+    }
+    controller.init(view, model)
+}
