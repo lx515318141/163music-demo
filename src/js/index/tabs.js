f17c855fddb6ee543322c6f323f4eb20331b085d
lixiang <515318141@qq.com>
2020-01-16 01:04:37 +0800

    tab切换

--- src/js/index/tabs.js
+++ src/js/index/tabs.js
@@ -13,12 +13,14 @@
             this.view.init()
             this.model = model
             this.bindEvents()
-
+            
         },
         bindEvents(){
             this.view.$el.on('click', '.tabs-nav > li', (e)=>{
                 let $li = $(e.currentTarget)
+                let tabName = $li.attr('data-tab-name')
                 $li.addClass('active').siblings().removeClass('active')
+                window.eventHub.emit('selectTab', tabName)
             })
         }
     }
