7472aa0a95c2cff19984e184d147c6f9c30fe39d
lixiang <515318141@qq.com>
2020-02-25 15:45:14 +0800

    完成歌曲滚动

--- src/js/song/play.js
+++ src/js/song/play.js
@@ -10,12 +10,17 @@
         .css("background-image", `url(${data.song.songinfo.pic_huge})`);
       this.$el.find("img.cover").attr("src", data.song.songinfo.pic_huge);
       if (this.$el.find("audio").attr("src") !== data.song.bitrate.file_link) {
+        // 防止每次暂停都重新渲染audio的src，会导致歌曲从头开始播放，所有先进行判断src中是否有该歌曲的播放链接，没有时才进行渲染
         let audio = this.$el
           .find("audio")
           .attr("src", data.song.bitrate.file_link)
           .get(0);
         audio.onended = () => {
+          // 歌曲播放完后触发ended事件，但此事件不冒泡，只能通过发布订阅来控制转盘停转
           window.eventHub.emit("songEnd");
+        };
+        audio.ontimeupdate = () => {
+          this.showLyric(audio.currentTime);
         };
       }
       if (data.status === "playing") {
@@ -29,21 +34,49 @@
         .find(".song-description>h1>.song_name")
         .text(data.song.songinfo.title);
       this.$el.find(".song-description>h1>b").text(data.song.songinfo.author);
-      let {lrcContent} = data.song.lyrics
-      console.log(lrcContent)
-      lrcContent.split('\n').map((string) => {
-        let p = document.createElement('p')
-        let regex = /\[([\d:.]+)\](.+)/
+      let { lrcContent } = data.song.lyrics;
+      lrcContent.split("\n").map(string => {
+        let p = document.createElement("p");
+        let regex = /\[([\d:.]+)\](.+)/;
         // 声明一个正则表达式
-        let matches = string.match(regex)
+        let matches = string.match(regex);
         // 将lrcContent里的string用正则表达式进行分组，若不符合正则表达式则不会进行分组
         // 将string的内容放在matches的第一项，[]里的放在第二项，[]外的放在第三项
-        if(matches){
-          p.textContent = matches[2]
-          p.setAttribute('data-time', matches[1])
+        if (matches) {
+          p.textContent = matches[2];
+          let parts = matches[1].split(":");
+          let time = parseInt(parts[0], 10) * 60 + parseFloat(parts[1], 10);
+          p.setAttribute("data-time", time);
+          // console.log('歌词')
+          // console.log(typeof this.$el.find(".lyric>.lines").html())
+          this.$el.find(".lyric>.lines").append(p);
         }
-        this.$el.find('.lyric>.lines').append(p)
-      })
+      });
+    },
+    showLyric(time) {
+      let allP = this.$el.find(".lyric>.lines>p");
+      let p
+      for (let i = 0; i < allP.length; i++) {
+        if(i === allP.length-1){
+          p = allP[i]
+          break
+        }else{
+          let currentTime = allP.eq(i).attr("data-time");
+          // jQuery的eq（）方法，eq选择器选取带有指定 index 值的元素,index值从0开始，
+          let nextTime = allP.eq(i + 1).attr("data-time");
+          if (currentTime <= time && time < nextTime) {
+            p = allP[i]
+            break
+          }  
+        }
+        }
+        let pHeight = p.getBoundingClientRect().top
+        let linesHeight = this.$el.find('.lyric>.lines')[0].getBoundingClientRect().top
+        let height = pHeight - linesHeight
+        this.$el.find('.lyric>.lines').css({
+          transform: `translateY(${-(height - 25)}px)`
+        })
+        $(p).addClass('active').siblings('.active').removeClass('active')
     },
     play() {
       this.$el.find("audio")[0].play();
@@ -84,10 +117,10 @@
           url: url
         });
       }
-      return getLrc(url).then((data) => {
-        this.data.song.lyrics = data
-        return this.data
-      })
+      return getLrc(url).then(data => {
+        this.data.song.lyrics = data;
+        return this.data;
+      });
     }
   };
   let controller = {
@@ -102,7 +135,7 @@
       });
       this.model.getSongLrc(id).then(data => {
         this.view.render(data);
-      })
+      });
       this.bindEvents();
     },
     bindEvents() {
